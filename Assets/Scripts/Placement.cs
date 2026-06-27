e1b221f753c1a2c956678a018b3eb390395068d4
José Boon <josejboon@gmail.com>
2026-06-27 15:08:39 +0200

    updated gitignore, added techtree template and placement script

---
+++ Assets/Scripts/Placement.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class Placement : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        RaycastHit hitInfo;
+        Ray ray = mainCamera.ScreenPointToRay(mousePosition);
+        if (!Physics.Raycast(ray, out hitInfo))
+        {
+            return;
+        }
+
+        transform.position = hitInfo.point;
+
+    }
+}
---
+++ Assets/Scripts/TechTree.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "TechTreeNode", menuName = "Scriptable Objects/TechTree")]
+public class TechTreeNode : ScriptableObject
+{ 
+    [SerializeField] private string _name = "Main";
+    [SerializeField] private bool _unlocked = false;
+    [SerializeField] private List<TechTreeNode> _nodes = new List<TechTreeNode>();
+    [SerializeField] private Sprite _icon;
+    [SerializeField] private float _buildTime;
+    [SerializeField] private GameObject _prefab;
+}
